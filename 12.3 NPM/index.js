3c0cb2b961f2eed1262a8869a2a1a69f793ed46f
Tal Shaked <73061047+talshaked-git@users.noreply.github.com>
2023-11-08 14:44:13 +0200

    finished basic node module, added express node files

---
+++ 12.3 NPM/index.js
@@ -0,0 +1,6 @@
+// import generateName from "sillyName"
+import superheroes from "superheroes"
+// var generateName = require("sillyname");
+// var sillyName = generateName();
+// console.log("My name is "+ generateName());
+console.log ("I am "+ superheroes.random());
