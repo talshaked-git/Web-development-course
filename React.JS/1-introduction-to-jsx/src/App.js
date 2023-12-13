417ce04115f035f3da907fce08f6645c1efe16db
Tal Shaked <talshaked94@gmail.com>
2023-12-13 18:24:14 +0200

    created react environment

---
+++ React.JS/1-introduction-to-jsx/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+      </header>
+      <main className="App-main"> main body </main>
+      <footer> footer </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ React.JS/1-introduction-to-jsx/src/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Services</li>
+        <li>Contact Us</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
