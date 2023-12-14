6e69a39d7ae9cb1dd6a39e77c09f8f26c0217577
Tal Shaked <talshaked94@gmail.com>
2023-12-14 15:52:32 +0200

    set up better work environment

---
+++ React.JS/2 JSX Code Practice/jsx.practice/src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello JSX Practice</h1>
+        <ul>
+          <li>Create a new React app.</li>
+          <li>Delete all of the code inside the App function.</li>
+          <li>In its place, render a single div element.</li>
+        </ul>
+        
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
