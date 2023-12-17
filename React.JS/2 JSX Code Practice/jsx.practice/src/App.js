1734f21b8b10b824bd0593aad962e8d1f75ed7a2
Tal Shaked <talshaked94@gmail.com>
2023-12-17 14:10:06 +0200

    finished lessons 2 to 9 (all in folder 2/app.js)

--- React.JS/2 JSX Code Practice/jsx.practice/src/App.js
+++ React.JS/2 JSX Code Practice/jsx.practice/src/App.js
@@ -1,19 +1,69 @@
 // import logo from './logo.svg';
 import './App.css';
 
+const name = "Tal";
+const number = 7;
+const customStyle = {
+  color: "blue",
+  fontSize: "50px",
+  border: "1px solid white"
+}
+
+const customStyle2 = {color: "blue"}
+var getYear = new Date().getFullYear();
+var getCurrentDay = new Date().getDay();
+var getCurrentHour = new Date().getHours();
+if (getCurrentDay === 0)
+{
+  customStyle.color = "red";
+}
+else
+{
+  customStyle.color = "blue";
+}
+var greeting = "";
+if (getCurrentHour < 12)
+{
+  greeting = "Good Morning";
+  customStyle2.color = "yellow";
+}
+else if (getCurrentHour < 18)
+{
+  greeting = "Good Afternoon";
+  customStyle2.color = "orange";
+}
+else
+{
+  greeting = "Good Evening";
+  customStyle2.color = "purple";
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello JSX Practice</h1>
+        <h1>JSX Practice</h1>
+        <h2 style={customStyle2}>{greeting}</h2>
         <ul>
-          <li>Create a new React app.</li>
-          <li>Delete all of the code inside the App function.</li>
-          <li>In its place, render a single div element.</li>
+          <li>My name is {name}</li>
+          <li>{name}'s lucky number is: {number}</li>
+          <li>The current year is: {getYear}</li>
+          <li style={customStyle}>{name}'s 3 favorite dishes:</li>
         </ul>
+        <div>
+          <img className="food-img" src="https://www.allrecipes.com/thmb/ULiSEmH8Tje7Hh-TW1aN2P8dC98=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/240376-homemade-pepperoni-pizza-Beauty-3x4-1-6ae54059c23348b3b9a703b6a3067a44.jpg" alt="pizza"></img>
+
+          <img className="food-img" src="https://www.mamaslebanesekitchen.com/wp-content/uploads/2011/02/chicken-shawarma-mamaslebanesekitchen1.jpg" alt="shawarma rolls"></img>
+
+          <img className="food-img" src="https://thestayathomechef.com/wp-content/uploads/2017/10/Chicken-and-Rice-Casserole-3.jpg" alt="rice and chicken"></img>
+
+        </div>
         
 
       </header>
+      <footer>
+        <p>Created by {name}</p>
+      </footer>
     </div>
   );
 }
