5a174d31d27cddd2087e42f50a3e8a5a9a07a1e2
Tal Shaked <talshaked94@gmail.com>
2023-12-21 12:27:21 +0200

    finished keeper app project part 2 (inside part 1 folder)

--- React.JS/13 Keeper App Part 1 Challenge/keeper_app/src/App.js
+++ React.JS/13 Keeper App Part 1 Challenge/keeper_app/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import notes from './notes';
 
 function App() {
   return (
     <div className="App">
      <Header />
-     <Note />
+     {notes.map(note => ( <Note key={note.key} title={note.title} content={note.content} /> ))}
 
 
       <Footer />
